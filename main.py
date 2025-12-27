18d913ffab396d82136a7213127e49ffa03b314a
Logan Bleyl <loganmbleyl05@gmail.com>
2025-12-26 20:22:45 -0500

    addedcalc

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,13 +13,16 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="AI Agent")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 
 
 
 response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents= args.user_prompt
+        contents= messages
                 )
 
 
@@ -31,6 +35,12 @@ if meta_data is None:
 prompt_token_count = meta_data.prompt_token_count
 candidates_token_count = meta_data.candidates_token_count
 
-print(f"Prompt tokens: {prompt_token_count}")
-print(f"Response tokens: {candidates_token_count}")
-print(response.text)
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidates_token_count}")
+    print(response.text)
+
+else:
+    print(response.text)
+    
